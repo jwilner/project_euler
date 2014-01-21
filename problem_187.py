f0a569c33a41936df2f7b6e7e86d41b44b1bff59
Joe Wilner <jhwilner1@gmail.com>
2014-01-20 19:32:02 -0500

    Solved problem 187.

---
+++ problem_187.py
@@ -0,0 +1,17 @@
+from problem_10 import get_all_primes_under
+
+
+def number_of_prime_factors(maximum):
+    primes = get_all_primes_under(maximum // 2)
+    count = 0
+    last_index = len(primes) - 1
+    for i, p in enumerate(primes):
+        while primes[last_index] * p > maximum:
+            if i > last_index:
+                return count + 1  # square of prime at i still be in bounds
+            last_index -= 1
+        count += last_index - i + 1
+
+if __name__ == '__main__':
+    import sys
+    print number_of_prime_factors(int(sys.argv[1]))
