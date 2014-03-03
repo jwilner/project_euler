5c0333fb240d0e8e75411c6301157c12d27758f4
Joe Wilner <jhwilner1@gmail.com>
2014-03-02 23:49:36 -0500

    The only hard part was dealing with the CSV...

--- pe_20_29/problem_22.py
+++ pe_20_29/problem_22.py
@@ -1,28 +1,17 @@
+import sys
 
-def return_word_value(name):
-    '''
-    takes a string and returns its 1-to-26 string value
-    problem22
-    '''
-    if(type(name) != str):
-        raise TypeError
 
-    value = 0
-    for character in name.lower():
-        value = value + ord(character)-96
-    return value
+def word_value(name):
+    return sum(ord(c) - 64 for c in name)
 
-def factorNumber(n):
-    '''
-    returns all factors of a number, including 1 and itself
-    problem 22
-    '''
-    factors = []
 
-    limit = math.floor(math.sqrt(n))
-    i = 1
-    while i <= limit:
-        if n % i == 0:
-            factors.extend([i,n//i])
-        i = i+1
-    return factors
+def score_words(words):
+    for i, word in enumerate(sorted(words), 1):
+        yield i * word_value(word)
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1]) as f:
+        words = f.read().replace('"', '').split(',')
+
+    print sum(score_words(words))
